3cd242119c85d9023169bfd5021fbbe692a62c4d
Naouwa <densali.nahla@gmail.com>
2023-04-20 22:04:08 +0000

    5-pritnf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a line with printf function
+ * return: 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
