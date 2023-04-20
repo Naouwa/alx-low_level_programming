80faf920fbcfa7f1ecb5ec10f7f198ad8fe4906f
Naouwa <densali.nahla@gmail.com>
2023-04-20 22:18:56 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - print a line with printf function
- * return: 0
+ * main - print line with printf function
+ * Return: 0
  */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
 }
