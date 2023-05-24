8af23846ce4f17484ff80008caa65cfc7e518157
Naouwa <densali.nahla@gmail.com>
2023-05-24 19:45:27 +0100

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: int array
+ * @size: size of array
+ * @cmp: compare function
+ * Return: integer index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]))
+			return (index);
+	}
+	return (-1);
+}
