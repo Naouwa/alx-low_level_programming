83086e3ac517700ef41c223b5ee99f569fad8567
Naouwa <densali.nahla@gmail.com>
2024-05-21 23:47:59 +0100

    mandatory tasks

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of integers
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ *
+ * Return: the index where value is located or -1 on failure
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t n, left, right;
+
+	if (array == NULL)
+		return (-1);
+
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (n = left; n < right; n++)
+			printf("%d, ", array[n]);
+		printf("%d\n", array[n]);
+
+		n = left + (right - left) / 2;
+		if (array[n] == value)
+			return (n);
+		if (array[n] > value)
+			right = n - 1;
+		else
+			left = n + 1;
+	}
+	return (-1);
+}
